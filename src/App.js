94525fef8d2054a6e75009d40a29904e0ee0bb37
Brooke Heck <b19heck@gmail.com>
2022-09-10 17:56:55 -0700

    working navbar

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './components/main/Home.js';
 import MyBooks from './components/main/MyBooks.js';
 import BookSearch from './components/main/BookSearch.js';
-import NoPage from './components/NoPage.js'
+import NoPage from './components/NoPage.js';
+import TopNav from './components/header/TopNav.js';
 
 export default function App() {
   return (
     <div className="App">
+      <TopNav/>
       <BrowserRouter>
         <Routes>
           <Route exact path='/' element={<Home />} />
