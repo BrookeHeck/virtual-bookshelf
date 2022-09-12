235977c4722c874a6d9df6e04c2a69e08d7aaade
Brooke Heck <b19heck@gmail.com>
2022-09-11 18:16:01 -0700

    authorization for backend requests

--- src/components/main/MyBooks.js
+++ src/components/main/MyBooks.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
-import { Container, Row, Col, Card, Button } from 'react-bootstrap';
+import { Container, Row, Col} from 'react-bootstrap';
 import axios from 'axios';
 import Book from './Book.js';
 import Filter from './Filter.js';
@@ -8,40 +8,39 @@ import './../../css/MyBooks.css';
 
 export default function MyBooks() {
   const [books, updateBooks] = useState([]);
-  const { isAuthenticated, getIdTokenClaims } = useAuth0();
-
+  const { isAuthenticated, getIdTokenClaims, user } = useAuth0();
   useEffect(() => {
-    if(isAuthenticated) {
+    if (isAuthenticated) {
       const getToken = async () => {
         let res = await getIdTokenClaims();
         return res.__raw;
       }
       try {
-        getToken().then(jwt => getBooks(jwt)).then(allBooks => updateBooks(allBooks));
-      } catch(e) {
+        getToken().then(jwt => getBooks(jwt, user)).then(allBooks => updateBooks(allBooks));
+      } catch (e) {
         console.log(e);
       }
     }
-  }, [isAuthenticated, getIdTokenClaims]);
+  }, [isAuthenticated, getIdTokenClaims, user]);
 
 
   return (
     <Container id='myBooksContainer'>
       <Row id="filterContainer">
-        <Filter/>
+        <Filter />
 
         <Col xs='9' lg='10'>
           <Container id="allBookDiv">
             {
-            books.map(book => {
-              return <Book
-                key={book._id}
-                title={book.title}
-                author={book.author}
-                notes={book.notes}
-                quotes={book.quotes}
-              />
-            })
+              books.map(book => {
+                return <Book
+                  key={book._id}
+                  title={book.title}
+                  author={book.author}
+                  notes={book.notes}
+                  quotes={book.quotes}
+                />
+              })
             }
           </Container>
         </Col>
@@ -50,17 +49,17 @@ export default function MyBooks() {
   );
 }
 
-async function getBooks(jwt) {
-  console.log(jwt);
+async function getBooks(jwt, user) {
   try {
     const config = {
       headers: { "Authorization": `Bearer ${jwt}` },
-        method: 'get',
-        baseURL: process.env.REACT_APP_SERVER,
-        url: '/books'
+      method: 'get',
+      baseURL: process.env.REACT_APP_SERVER,
+      url: '/books',
+      params: {email: user.email}
     }
     let res = await axios(config);
-    return res.data.userBooks;
+    return res.data[0].userBooks;
   } catch (e) {
     console.log(e);
   }
